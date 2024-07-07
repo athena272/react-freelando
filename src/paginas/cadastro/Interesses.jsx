3c1d954c5d3f9a2a76a5d57ea71ee164be0116ee
athena272 <guilhermera272@gmail.com>
2024-07-07 17:26:59 -0300

    Update Interesses.jsx

--- src/paginas/cadastro/Interesses.jsx
+++ src/paginas/cadastro/Interesses.jsx
@@ -4,6 +4,7 @@ import { Col, Row } from "react-grid-system"
 import { Botao } from "../../componentes/Botao/Botao"
 import { Link } from "react-router-dom"
 import { useState } from "react"
+import { Form, Formik } from "formik"
 
 const opcoes = [
     {
@@ -33,8 +34,6 @@ const opcoes = [
 ]
 
 const Interesses = () => {
-    const [interesse, setInteresse] = useState('')
-
 
     return (<>
         <div style={{ textAlign: 'center' }}>
@@ -45,11 +44,15 @@ const Interesses = () => {
                 Qual a área de interesse?
             </Tipografia>
         </div>
-        <GrupoRadio
-            opcoes={opcoes}
-            valor={interesse}
-            onChange={setInteresse}
-        />
+        <Formik initialValues={{
+            interesse: ''
+        }}>
+            <Form>
+                <GrupoRadio
+                    opcoes={opcoes}
+                />
+            </Form>
+        </Formik>
         <Row>
             <Col lg={6} md={6} sm={6}>
                 <Link to="/cadastro">
